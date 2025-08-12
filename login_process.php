10a07ebf16bc1fbea2446a9e057f62d8051723ee
Dennis Alminaza <dennisitlab@gmail.com>
2025-08-12 22:17:02 +0800

    Update login_process.php
    Process for login. Php that connects to our database

--- login_process.php
+++ login_process.php
@@ -1,1 +1,20 @@
+<?php
+
+$conn = new mysqli("localhost", "root", "", "collab_db");
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE email='$email'";
+$result = $conn->query($sql);
+
+if ($result && $result->num_rows > 0) {
+    echo "Login Successful!";
+} else {
+    echo "Invalid Login.";
+}
+
+$conn->close();
+
+?>
 
