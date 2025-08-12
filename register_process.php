f89a11c67c06ee8c35c1f3fbe34345e5d2f64329
Dennis Alminaza <dennisitlab@gmail.com>
2025-08-12 22:31:03 +0800

    Update register_process.php
    Php process for our register. This will connect to our database also

--- register_process.php
+++ register_process.php
@@ -1,1 +1,25 @@
+<?p
+  
+$conn = new mysqli("localhost", "root", "", "collab_db");
 
+$fullname = $_POST['fullname'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$confirm_password = $_POST['confirm_password'];
+
+if ($password !== $confirm_password) {
+    die("Passwords do not match.");
+}
+
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO users (fullname, email, password) VALUES ('$fullname', '$email', '$hashed_password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Registration successful! <a href='login.php'>Login here</a>";
+} else {
+    echo "Error: " . $conn->error;
+}
+
+$conn->close();
+?>
